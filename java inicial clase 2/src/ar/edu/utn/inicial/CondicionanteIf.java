54c800fe0b76954f1a60827c4ae1cfae6cd3bb8e
PabloS <pabloesayago@gmail.com>
2019-03-20 22:06:02 -0300

    Modificacion del if

--- java inicial clase 2/src/ar/edu/utn/inicial/CondicionanteIf.java
+++ java inicial clase 2/src/ar/edu/utn/inicial/CondicionanteIf.java
@@ -4,14 +4,15 @@ public class CondicionanteIf {
 	
 	public static void main(String[] args) {
 		
-		int valor = 1;
-		
+		int valor = -1;
+		//comentario
 		if(valor < 0) {
-			// bloque de codigo si se cumple
-			// la condicion
 			System.out.println("Valor es negativo");			
+		} else if (valor>0) {
+			System.out.println("Valor es positivo");
 		}
-		else System.out.println("Valor es positivo");		
+		else System.out.println("Valor es cero");
+		System.out.println("El valor es: " + valor + " <= ese es su valor");
 	}	
 	
 }
