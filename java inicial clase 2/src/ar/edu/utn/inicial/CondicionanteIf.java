f055a8666e0aa9d0493a23521948d1989ca7b2ea
Pablo S <pabloesayag@gmail.com>
2019-03-20 21:42:26 -0300

    Subo la clase con el git

---
+++ java inicial clase 2/src/ar/edu/utn/inicial/CondicionanteIf.java
@@ -0,0 +1,17 @@
+package ar.edu.utn.inicial;
+
+public class CondicionanteIf {
+	
+	public static void main(String[] args) {
+		
+		int valor = 1;
+		
+		if(valor < 0) {
+			// bloque de codigo si se cumple
+			// la condicion
+			System.out.println("Valor es negativo");			
+		}
+		else System.out.println("Valor es positivo");		
+	}	
+	
+}
