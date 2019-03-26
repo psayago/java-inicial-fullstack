597bdb73b9881fdfe40abd7f5b931810453483db
Pablo Sayago <pabloesayago@gmail.com>
2019-03-25 21:25:25 -0300

    Practica Switch Case

---
+++ java inicial clase 2/src/ar/edu/utn/inicial/TestSwitchCase.java
@@ -0,0 +1,25 @@
+package ar.edu.utn.inicial;
+
+public class TestSwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char color='M';
+	    switch (color) {
+	    case 'R': 
+	    	System.out.println("Color ROJO");
+	    	break;	    
+	    case 'B': 
+	    	System.out.println("Color AZUL");
+	    	break;
+	    case 'Y': 
+	    	System.out.println("Color AMARILLO");
+	    	break;
+	    default:
+	    	System.out.println("No reconozco el color");
+	    	break;
+	    }
+	    
+	}
+
+}
