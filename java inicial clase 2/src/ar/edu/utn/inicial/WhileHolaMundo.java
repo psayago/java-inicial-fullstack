ef5082243a7c11ec129cf1063e60a03efe2ce97f
SebasAKD <sebastian.minici@gmail.com>
2019-03-27 20:17:19 -0300

    Practica FOR clase 5

---
+++ java inicial clase 2/src/ar/edu/utn/inicial/WhileHolaMundo.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.inicial;
+
+public class WhileHolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while (i!=22) {
+				System.out.println("Hola Mundo");	
+				i++;
+			}
+		}	
+}
